3b9608605e3430e484ae02eb45daeb4f10d4f2c4
ovieu <ovie.u.umukoro@gmail.com>
2018-05-30 19:16:05 -0400

    Add instance variables and update constructor

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,14 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
