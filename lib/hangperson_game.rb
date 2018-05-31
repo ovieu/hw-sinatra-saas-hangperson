5b0112b056a618b5a3bb41ae1a618e5ad5022922
ovieu <ovie.u.umukoro@gmail.com>
2018-05-31 15:41:38 -0400

    Repeated same letter returns false

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -9,13 +9,15 @@ class HangpersonGame
   # Updates the correct guess word and returns true if the user
   # enters the correct guess, otherwise returns false
   def guess(letter)
-    if !@guesses.include?(letter) &&  !@wrong_guesses.include?(letter)
+    if !@guesses.include?(letter) && !@wrong_guesses.include?(letter)
         if @word.include?(letter)
           @guesses << letter
         else 
           @wrong_guesses << letter
         end
         return true
+    else
+      return false
     end
   end
 
