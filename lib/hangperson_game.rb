fd7a1ee6994ea134579bcc618d09a303af081a34
ovieu <ovie.u.umukoro@gmail.com>
2018-05-31 14:25:27 -0400

    Refactor Hangperson_game file for structure

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,20 +2,9 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
   attr_accessor :word
   attr_accessor :guesses
   attr_accessor :wrong_guesses
-
-  def initialize(word)
-    @word = word
-    @guesses = ''
-    @wrong_guesses = ''
-  end
 
   # Updates the correct guess word and returns true if the user
   # enters the correct guess, otherwise returns false
@@ -23,6 +12,16 @@ class HangpersonGame
     @word.include?(letter) ?  @guesses << letter :  @wrong_guesses << letter
     return true
   end
+
+  # Get a word from remote "random word" service
+
+  # def initialize()
+  def initialize(word)
+    @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
